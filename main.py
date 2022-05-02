10ffc3346ef0fc53746e34185eb555c04969a321
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-05-02 20:20:15 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# I saw an exercise on the internet asking you to create a program
+# which would take an input from the user (a number from 1 to 9) and
+# as output would display a list of super prime numbers (numbers that
+# are prime and, at the same time, that all its digits are prime). Those
+# super prime numbers must be presented in ascending order, and must
+# have a certain number of digits (such number of digits being the number
+# entered as input).
+
+list_a = ["3", "5", "7"]
+list_b = ["0", "2", "3", "5", "7"]
+number = ""
